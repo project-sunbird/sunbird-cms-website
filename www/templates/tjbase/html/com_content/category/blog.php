0a8c724500af3510110f66984d2fdbbcd38528bd
Pallavi Patil <pallavi_p@tekditechnologies.com>
2020-05-19 14:54:08 +0530

    Issue #04 chore: Website changes as per new design

--- www/templates/tjbase/html/com_content/category/blog.php
+++ www/templates/tjbase/html/com_content/category/blog.php
@@ -128,7 +128,7 @@ $afterDisplayContent = trim(implode("\n", $results));
 			<?php echo $this->loadTemplate('children'); ?> </div>
 	<?php endif; ?>
 	<?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
-		<div class="pagination">
+		<div class="pagination-wrap">
 			<?php if ($this->params->def('show_pagination_results', 1)) : ?>
 				<p class="counter pull-right"> <?php echo $this->pagination->getPagesCounter(); ?> </p>
 			<?php endif; ?>
